8b4fdc99d13e90b24292102093706f0b7478149e
tonyekim <otoobongekim@gmail.com>
2023-05-04 22:27:35 +0100

    updates

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,32 +1,21 @@
 #!/usr/bin/node
-// using star wars API
+const request = require("request");
+const movieId = process.argv[2];
+const apiUrl = "https://swapi-api.hbtn.io/api/films/" + movieId;
 
-const request = require('request');
-const FILMID = process.argv[2];
-
-// Request URL
-const URL_BASE = 'https://swapi-api.hbtn.io/api/films';
-
-function doRequest (url) {
-  return new Promise(function (resolve, reject) {
-    request(url, function (error, res, body) {
-      if (!error && res.statusCode === 200) {
-        resolve(JSON.parse(body));
-      } else {
-        reject(error);
-      }
-    });
-  });
-}
-
-// Usage:
-
-async function main (filmID) {
-  const res = await doRequest(`${URL_BASE}/${filmID}`);
-  for (const e of res.characters) {
-    const pj = await doRequest(e);
-    console.log(pj.name);
+request(apiUrl, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const charactersUrls = JSON.parse(body).characters;
+    for (const characterUrl of charactersUrls) {
+      request(characterUrl, function (error, response, body) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log(JSON.parse(body).name);
+        }
+      });
+    }
   }
-}
-
-main(FILMID);
+});
