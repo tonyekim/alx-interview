233571c77959fc6f48e7b35199c424192523e948
tonyekim <otoobongekim@gmail.com>
2023-05-18 21:40:46 +0000

    Add function that uses dynamic programming to determine the minimum n

---
+++ 0x08-making_change/test_files/0-main.py
@@ -0,0 +1,10 @@
+in file for testing
+"""
+
+makeChange = __import__('0-making_change').makeChange
+
+print(makeChange([1, 2, 25], 37))
+
+print(makeChange([1256, 54, 48, 16, 102], 1453))
+
+print(makeChange([3, 25], 52))
