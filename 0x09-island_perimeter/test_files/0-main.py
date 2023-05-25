74cfdfc88bc7c100e362dd9378e98465db6020bb
tonyekim <otoobongekim@gmail.com>
2023-05-25 05:52:40 +0000

    A function that calculate the perimeter of an island

---
+++ 0x09-island_perimeter/test_files/0-main.py
@@ -0,0 +1,38 @@
+n
+"""
+island_perimeter = __import__('0-island_perimeter').island_perimeter
+
+if __name__ == "__main__":
+    grid = [
+        [0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0]
+    ]
+    print('12')
+    print(island_perimeter(grid))
+    grid = [
+        [0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 1, 0, 0],
+        [0, 1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0]
+    ]
+    print('14')
+    print(island_perimeter(grid))
+    grid = [
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0]
+    ]
+    print('0')
+    print(island_perimeter(grid))
+    grid = [
+        [0, 1, 1, 1, 1, 0],
+        [0, 0, 0, 0, 0, 0]
+    ]
+    print('10')
+    print(island_perimeter(grid))
